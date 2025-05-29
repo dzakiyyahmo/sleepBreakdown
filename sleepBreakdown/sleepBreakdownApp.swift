b4de94e8703cb78a9f38b509be648db21d615512
Dzakiyyah Azahra <dmooduto24@gmail.com>
2025-05-29 14:11:27 +0800

    Initial Commit

---
+++ sleepBreakdown/sleepBreakdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  sleepBreakdownApp.swift
+//  sleepBreakdown
+//
+//  Created by Dzakiyyah Azahra on 29/05/25.
+//
+
+import SwiftUI
+
+@main
+struct sleepBreakdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
